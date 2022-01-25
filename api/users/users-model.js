02083f9a2164adf1b7c985b39eca0b363c54c1b1
adamhinton <adam.d.hinton@gmail.com>
2022-01-25 18:02:16 -0500

    wip

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -65,6 +65,7 @@ function findById(user_id) {
       "role_name": "instructor"
     }
    */
+
   return db("users")
     .join("roles", "users.role_id", "roles.role_id")
     .select("user_id", "username", "role_name")
